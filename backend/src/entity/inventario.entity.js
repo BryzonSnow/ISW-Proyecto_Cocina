d743c4884593e0141e7fd0a8d0295ce0bf58483a
nebcore <victor.aguilera2102@alumnos.ubiobio.cl>
2024-10-30 07:25:29 -0300

    insersion entitys

---
+++ backend/src/entity/inventario.entity.js
@@ -0,0 +1,21 @@
+import { EntitySchema } from "typeorm";
+
+const InventarioSchema = new EntitySchema({
+    name: "Inventario",
+    tableName: "Inventario",
+    columns: {
+        InventarioID: { type: "int", primary: true, generated: true },
+        Fecha: { type: "date", nullable: false },
+        CantidadTotal: { type: "int", nullable: false },
+        Estado: { type: "varchar", length: 50, nullable: false }
+    },
+    relations: {
+        administrador: {
+            type: "many-to-one",
+            target: "Administrador",
+            joinColumn: { name: "AdministradorID" }
+        }
+    }
+});
+
+export default InventarioSchema;
