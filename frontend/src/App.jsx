44414fbc646889c3f1764a45b229707fb269943a
Omar Castro González <70120926+AshCrow13@users.noreply.github.com>
2024-11-27 13:53:52 -0300

    Update App.jsx
    import proveedores duplicados

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Ingrediente from './components/Ingrediente';
 import Inicio from './components/Inicio';
 import Menu from './components/Menu';
 import Inventario from './components/Inventario';
-import Proveedores from './components/Proveedores';
 import WspBubble from './components/wspbubble';
 import Footer from './components/Footer';
 import Gestionmenu from './components/Gestionmenu';
@@ -20,7 +19,7 @@ const App = () => {
         <Route path="/gestionmenu" element={<Gestionmenu />} />
         <Route path="/Ingrediente" element={<Ingrediente />} />
         <Route path="/Inventario" element={<Inventario />} />
-        <Route path="/proveedores" element={<Proveedores />} />
+        <Route path="/proveedor" element={<Proveedor />} />
       </Routes>
       <Footer />
       <WspBubble />
